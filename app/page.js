419c58d2981e0541a24a380e999cb94ed2f0aca6
Mrityunjay <mratyunjaytiwari2114@gmail.com>
2026-07-15 23:35:37 +0530

    user onboarding implemented

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { onBoardUser } from "@/module/auth/actions";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await onBoardUser();
   return (
     <>
-    <div>page : index</div>
-    <Button>Click me !</Button>
+    <UserButton></UserButton>
     </>
   );
 }
